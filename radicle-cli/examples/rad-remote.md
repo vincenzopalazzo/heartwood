0d0de8c59007959dfa0fb03bbc70ee67b04d4440
Slack Coder <slackcoder@server.ky>
2023-04-04 15:27:34 -0500

    radicle-cli: Use `rad remote` to list rad remotes
    Help users by providing `rad remote` as a utility to list and operate on
    radicle remotes.
    
    Repurpose an unused method `radicle_cli::git::remotes()` to do what its
    comment suggests, list a project's radicle remotes.  Use it to list the
    remotes to the user.
    
    An example file is provided to demonstrate the commands use.  It is
    intended to be temperary until the command is completed and the
    documentation better incorporated into the project.

---
+++ radicle-cli/examples/rad-remote.md
@@ -0,0 +1,12 @@
+`rad remote` can help with managing git's radicle remotes.
+
+Let's take a look at the radicle remotes we have from cloning a project.
+
+```
+$ rad remote -v
+rad rad://z42hL2jL4XNk6K8oHQaSWfMgCL7ji/z6Mkt67GdsW7715MEfRuP4pSZxJRJh6kj6Y48WRqVv4N1tRk (fetch)
+z6MknSLrJoTcukLrE435hVNQT4JUhbvWLX4kUzqkEStBU8Vi    rad://z42hL2jL4XNk6K8oHQaSWfMgCL7ji/z6MknSLrJoTcukLrE435hVNQT4JUhbvWLX4kUzqkEStBU8Vi (fetch)
+```
+
+The remote `rad` is our fork of the project, and
+'z6MknSLrJoTcukLrE435hVNQT4JUhbvWLX4kUzqkEStBU8Vi' is the origin's.
--- radicle-cli/src/commands/remote/list.rs
+++ radicle-cli/src/commands/remote/list.rs
@@ -1,7 +1,30 @@
-use radicle::storage::git::Repository;
-use radicle::Profile;
+use super::Options;
+use crate::git;
+
+#[inline]
+fn format_direction(d: &git::Direction) -> &str {
+    match d {
+        git::Direction::Fetch => "fetch",
+        git::Direction::Push => "push",
+    }
+}
 
 /// core command to run the `rad remote list` or just `rad remote` subcommand.
-pub fn run(repository: &Repository, profile: &Profile) -> anyhow::Result<()> {
+pub fn run(repo: &git::Repository, options: &Options) -> anyhow::Result<()> {
+    let remotes = git::rad_remotes(repo)?;
+    for r in remotes {
+        if options.verbose {
+            for spec in r.refspecs() {
+                println!(
+                    "{}\t{} ({})",
+                    r.name().unwrap(),
+                    r.url().unwrap(),
+                    format_direction(&spec.direction()),
+                );
+            }
+        } else {
+            println!("{}", r.name().unwrap());
+        }
+    }
     Ok(())
 }
