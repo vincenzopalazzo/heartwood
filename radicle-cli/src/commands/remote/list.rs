73869d3bdbcd2c3a36e3b8ce78e029a50e868a95
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-05 12:22:17 +0200

    feat(remote): improve the print of the remote list cmd
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- radicle-cli/src/commands/remote/list.rs
+++ radicle-cli/src/commands/remote/list.rs
@@ -1,30 +1,33 @@
+use radicle_term::{Element, Table};
+
 use super::Options;
 use crate::git;
+use crate::terminal as term;
 
 #[inline]
-fn format_direction(d: &git::Direction) -> &str {
+fn format_direction(d: &git::Direction) -> String {
     match d {
-        git::Direction::Fetch => "fetch",
-        git::Direction::Push => "push",
+        git::Direction::Fetch => "fetch".to_owned(),
+        git::Direction::Push => "push".to_owned(),
     }
 }
 
 /// core command to run the `rad remote list` or just `rad remote` subcommand.
-pub fn run(repo: &git::Repository, options: &Options) -> anyhow::Result<()> {
+pub fn run(repo: &git::Repository, _: &Options) -> anyhow::Result<()> {
+    let mut table = Table::default();
     let remotes = git::rad_remotes(repo)?;
     for r in remotes {
-        if options.verbose {
-            for spec in r.refspecs() {
-                println!(
-                    "{}\t{} ({})",
-                    r.name().unwrap(),
-                    r.url().unwrap(),
-                    format_direction(&spec.direction()),
-                );
-            }
-        } else {
-            println!("{}", r.name().unwrap());
+        for spec in r.refspecs() {
+            let dir = spec.direction();
+            let url = r.url().unwrap();
+            let name = r.name().unwrap();
+            table.push([
+                term::format::badge_positive(format_direction(&dir)),
+                term::format::highlight(name.to_owned()),
+                term::format::italic(url.to_owned()),
+            ]);
         }
     }
+    table.print();
     Ok(())
 }
